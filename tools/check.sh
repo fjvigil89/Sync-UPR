922074f11de179b4019f0298b4633c140c62d34d
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-05-28 11:25:16 -0400

    creando CI para uso generico dde laravel

---
+++ tools/check.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+ENV=${1:-master}
+echo Deploying $ENV
+ssh -i /root/.ssh/id_rsa root@10.2.24.193 'cd /home/Sync-UPR/master && sudo cp .env.example .env && sudo php artisan key:generate'
+
+
