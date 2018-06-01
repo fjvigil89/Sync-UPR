80157f2de981a01c797f7332f89e78594d516bc1
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-06-01 15:30:10 -0400

    Hecho bajas diarias de ad

--- tools/check.sh
+++ tools/check.sh
@@ -2,6 +2,6 @@
 
 ENV=${1:-master}
 echo Deploying $ENV
-ssh -i /root/.ssh/id_rsa root@10.2.24.193 'cd /home/Sync-UPR/master && sudo cp .env.example .env && sudo php artisan key:generate && sudo chmod -R 777 storage'
+ssh -i /root/.ssh/id_rsa root@10.2.24.192 'cd /home/Sync-UPR/master && sudo cp .env.example .env && sudo php artisan key:generate && sudo chmod -R 777 storage'
 
 
