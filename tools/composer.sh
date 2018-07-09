58937f544770862516fc004165458c9dd5e968d3
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-07-09 09:49:14 -0400

    quitando el gitlkab-running para esta version

--- tools/composer.sh
+++ tools/composer.sh
@@ -2,5 +2,5 @@
 
 ENV=${1:-master}
 echo Deploying $ENV
-ssh -i /root/.ssh/id_rsa root@10.2.24.192 'cd /home/Sync-UPR/master && sudo php ../../composer.phar update --prefer-dist -o'
+#ssh -i /root/.ssh/id_rsa root@10.2.24.192 'cd /home/Sync-UPR/master && sudo php ../../composer.phar update --prefer-dist -o'
 
