edb459c8d34319e4f55e955f89eee3c414a47d2a
Frank J. Vigil Vega <frank.vigil@upr.edu.cu>
2018-07-09 10:16:55 -0400

    montandolo en el server

--- tools/swagger.sh
+++ tools/swagger.sh
@@ -2,5 +2,5 @@
 
 ENV=${1:-master}
 echo Deploying $ENV
-#ssh -i /root/.ssh/id_rsa root@10.2.24.192 'cd /home/Sync-UPR/master && sudo ./vendor/bin/swagger app/Http --output public/api/'
+ssh -i /root/.ssh/id_rsa root@10.2.24.192 'cd /home/Sync-UPR/master && sudo ./vendor/bin/swagger app/Http --output public/api/'
 
